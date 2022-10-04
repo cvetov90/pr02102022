f8a504eb52b2f3e438459514b32ccd1a60851fc4
cvetov <cvetov90@mail.ru>
2022-10-04 08:45:30 +0400

    v1 без моделей

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,6 @@ export default {
 
 <style>
 #app {
-  margin: 0;
-  padding: 0;
-  left: 0;
   /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
